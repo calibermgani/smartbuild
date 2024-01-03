41e8759b5f0069cdc5fa803ffa66e492759c3e41
Loganayaki <loganayaki@caliberfocus.com>
2024-01-03 11:02:38 +0530

    Relationship foreign key updated

--- smartbuild_api/app/Models/Material/Material.php
+++ smartbuild_api/app/Models/Material/Material.php
@@ -5,11 +5,15 @@ namespace App\Models\Material;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Category\Category;
+use App\Models\Size\Size;
+use App\Models\Vendor\Vendor;
+use App\Models\Unit\Unit;
 
 class Material extends Model
 {
     use HasFactory,SoftDeletes;
-    
+
     protected $fillable = [
         'item_no', 'item_name', 'items_category', 'item_description', 'procedure',
         'cat_no', 'lot_no', 'size', 'vendor', 'price', 'unit', 'expiry_date',
@@ -22,4 +26,27 @@ class Material extends Model
         'items_category' => 'required',
        
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'items_category');
+    }
+
+    // Relationship with Size
+    public function size()
+    {
+        return $this->belongsTo(Size::class, 'size');
+    }
+
+    // Relationship with Vendor
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class, 'vendor');
+    }
+
+    // Relationship with Unit
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit');
+    }
 }
