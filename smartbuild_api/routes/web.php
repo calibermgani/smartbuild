086afc3bb94741ca56a8aa303a6e99b06ef38a87
Loganayaki <loganayaki@caliberfocus.com>
2024-01-04 19:09:00 +0530

    backend updated

--- smartbuild_api/routes/web.php
+++ smartbuild_api/routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Category\CategoryController;
-use App\Http\Controllers\Unit\UnitController;
-use App\Http\Controllers\Vendor\VendorController;
-use App\Http\Controllers\Size\SizeController;
-use App\Http\Controllers\Material\MaterialController;
-use App\Http\Controllers\Menu\MenuController;
-use App\Http\Controllers\Submenu\SubmenuController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,12 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('units')->group(function () {
-    Route::get('/', [UnitController::class, 'index']);
-    Route::post('/', [UnitController::class, 'store']);
-    Route::get('/{id}', [UnitController::class, 'show']);
-    Route::put('/{id}', [UnitController::class, 'update']);
-    Route::delete('/{id}', [UnitController::class, 'destroy']);
-});
+
 
 
