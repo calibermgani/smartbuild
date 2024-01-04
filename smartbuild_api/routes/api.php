987f8ea48d9fc2e3a30d07c0319d6dc7bca94929
Loganayaki <loganayaki@caliberfocus.com>
2024-01-04 12:39:30 +0530

    Testing

--- smartbuild_api/routes/api.php
+++ smartbuild_api/routes/api.php
@@ -53,7 +53,7 @@ Route::prefix('vendors')->group(function () {
 });
 
 Route::prefix('sizes')->group(function () {
-    dd('Testing');
+   
     Route::get('/', [SizeController::class, 'index']);
     Route::post('/', [SizeController::class, 'store']);
     Route::get('/{id}', [SizeController::class, 'show']);
